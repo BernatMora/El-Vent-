ee4f6b7aefb6ab68f1e9530d4dbf2c6863e6353d
BernatMora <bernatmora@mac.com>
2026-04-10 09:37:13 +0200

    fix: eliminar SW, només unregister i clear caches

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,14 +37,11 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               if ('serviceWorker' in navigator) {
-                window.addEventListener('load', () => {
-                  navigator.serviceWorker.getRegistrations().then(regs => {
-                    regs.forEach(r => r.unregister())
-                  }).then(() => {
-                    caches.keys().then(keys => keys.forEach(k => caches.delete(k)))
-                  }).then(() => {
-                    navigator.serviceWorker.register('/sw.js')
-                  })
+                navigator.serviceWorker.getRegistrations().then(function(regs) {
+                  regs.forEach(function(r) { r.unregister() })
+                })
+                caches.keys().then(function(keys) {
+                  keys.forEach(function(k) { caches.delete(k) })
                 })
               }
             `,
